19b05a1dfcf12659469a96b8bd72fc91a134d5d3
georgecampanis <georgecampanis@gmail.com>
2021-12-08 13:57:12 -0400

    Task Scheduler scrip

---
+++ passingpara.ps1
@@ -0,0 +1,19 @@
+# script file passingpara.ps1 
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$firstname = "George",
+
+    [Parameter(Mandatory=$true)]
+    [string]$lastname = "Campanis"
+)
+
+
+$datetimeout = Get-Date
+
+$mystring= $firstname + " " + $lastname + "     " + $datetimeout
+
+$mystring >> C:\temp\outfile.txt
+
+# end of script file
+
